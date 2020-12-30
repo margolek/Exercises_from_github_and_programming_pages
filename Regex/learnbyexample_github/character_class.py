aa2e2a746f3dcc3b5eb5d1fb66ff93b5328ea5ab
margolek <margolek555@gmail.com>
2020-12-30 20:37:49 +0100

    Update

--- Regex/learnbyexample_github/character_class.py
+++ Regex/learnbyexample_github/character_class.py
@@ -64,6 +64,7 @@ def extract(input_string):
 	output:
 	['128A', '0xfe32', '34']
 	['0XDEADBEEF', '0x0ff1ce', 'bad']
+
 	"""
 	regex = re.compile(r'\b(0x)?[\da-f]+\b',re.I)
 	#0 in x[0] means whole part of chosen pattern
@@ -127,7 +128,6 @@ def replace2(input_string):
 	"""
 	regex = re.compile(r'(,[^,]*){3}$')
 	return regex.sub(',WHTSZ323',input_string)
-
 def split(input_string):
 	"""
 	Split the given strings based on consecutive
@@ -141,4 +141,115 @@ def split(input_string):
 	['lion', 'Ink', 'onion', 'Nice']
 	['**', 'star', '**']
 	"""
+	regex = re.compile(r'[\s\d]+')
+	return regex.split(input_string)
+def delete(input_string):
+	"""
+	Delete all occurrences of the sequence <characters> where characters
+	is one or more non > characters and cannot be empty.
+
+	input:
+	>>> ip = 'a<apple> 1<> b<bye> 2<> c<cat>'
+
+	output:
+	'a 1<> b 2<> c'
+	"""
+	regex = re.compile(r'<[^>]+>')
+	return regex.sub('', input_string)
+def filter6(input_list):
+	"""
+	For the given list, filter all elements containing
+	any number sequence greater than 624.
+
+	input:
+	>>> items=['hi0000432abcd', 'car00625', '42_624 0512','3.14 96 2 foo1234baz']
+	
+	output:
+	['car00625', '3.14 96 2 foo1234baz']
+	"""
+	#focus more on this example
+	return [e for e in input_list if any(int(m[0])>624 for m in re.finditer(r'\d+', e))]
+def max_nested_braces(ip):
+	"""
+	Count the maximum depth of nested braces for the given strings.
+	Unbalanced or wrongly ordered braces should return '-1'.
+	Note that this will require a mix of regular expressions and Python code.
+
+	input:
+	'a*b'
+	'}a+b{'
+	'a*b+{}'
+	'{{a+2}*{b+c}+e}'
+	'{{a+2}*{b+{c*d}}+e}'
+	'{{a+2}*{\n{b+{c*d}}+e*d}}'
+	'a*{b+c*{e*3.14}}}'
+
+	output:
+	0
+	-1
+	1
+	2
+	3
+	4
+	-1
+	"""
+	regex = re.compile(r'\{[^{}]*\}')
+	regex_alt = re.compile(r'[{}]')
+	count = 0
+	while True:
+		ip,subs_num =  regex.subn('', ip)
+		if subs_num == 0:
+			break
+		count+=subs_num
+	if regex_alt.search(ip):
+		return -1
+	return count
+def check_point(ip):
+	"""
+	By default, 'str.split' method will split on whitespace
+	and remove empty strings from the result.
+	Which 're' module function would you use to replicate this functionality?
+	
+	input:
+	>>> ip = ' \t\r  so  pole\t\t\t\n\nlit in to \r\n\v\f  '
+
+	output:
+	split()
+	['so', 'pole', 'lit', 'in', 'to']
+	Add solution here
+	['so', 'pole', 'lit', 'in', 'to']
+	"""
+	regex = re.compile(r'\S+')
+	return regex.findall(ip)
+def convert(ip):
+	"""
+	Convert the given input string to two different lists as shown below.
+	
+	input:
+	ip = 'price_42 roast^\t\n^-ice==cat\neast'
+
+	output:
+	['price_42', 'roast', 'ice', 'cat', 'east']
+	['price_42', ' ', 'roast', '^\t\n^-', 'ice', '==', 'cat', '\n', 'east']
+	"""
+	regex1 = re.compile(r'[\s==^-]+')
+	regex2 = re.compile(r'([\s==^-]+)')
+	#put question about it
+	return regex1.split(ip),regex2.split(ip)
+def filter7(ip):
+	"""
+	Filter all elements whose first non-whitespace
+	character is not a # character.
+	Any element made up of only whitespace characters should be ignored as well.
+
+	input:
+	items = ['    #comment', '\t\napple #42', '#oops', 'sure', 'no#1', '\t\r\f']
+
+	output:
+	['\t\napple #42', 'sure', 'no#1']
+	"""
+	regex = re.compile(r'\A\s*[^#\s]')
+	#why \s in own class?
+	return [x for x in ip if regex.search(x)]
+
 
