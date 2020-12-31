3b63aa597573237d7e8733bb7e42b33c72bc237e
margolek <margolek555@gmail.com>
2020-12-31 23:50:25 +0100

    Update

--- Regex/detect_strong_password.py
+++ Regex/detect_strong_password.py
@@ -14,8 +14,21 @@ special characters from !@#$%^&*?
 import re
 
 def check_password(s):
-	
+	regex1 = re.compile(r'[A-Z]')
+	regex2 = re.compile(r'[a-z]')
+	regex3 = re.compile(r'[0-9]')
+	regex4 = re.compile(r'[!@#?$%&*^]')
+	regex5 = re.compile(r'[^a-zA-Z0-9!@#?$%&*^]')
+
+	if regex1.search(s) and regex2.search(s) and regex3.search(s) and regex4.search(s) and not regex5.search(s):
+		if len(s) >= 8 and len(s) <=20:
+			return 'valid'
+		else:
+			return 'not valid'
+	else:
+		return 'not valid'
 
 
-a = check_password('P1@pP1@p')
+
+a = check_password('valid')
 print(a)
