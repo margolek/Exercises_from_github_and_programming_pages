9ebc7afb48312dde34978291697bd3e47a2425bc
margolek <margolek555@gmail.com>
2021-01-02 12:54:13 +0100

    Update grouping_and_backreferences.py

--- Regex/learnbyexample_github/grouping_and_backreferences.py
+++ Regex/learnbyexample_github/grouping_and_backreferences.py
@@ -106,6 +106,112 @@ def replace3(ip):
 	output:
 	'1234 23 4535 9339 1 60260260'
 	"""
+	regex = re.compile(r'\b(\d+)\1+\b')
+	return regex.sub('\g<1>',ip),regex.findall(ip)
+def replace4(ip):
+	"""
+	Replace sequences made up of words separated by ':' or '.' by
+	the first word of the sequence. Such sequences will end when ':' or '.'
+	is not followed by a word character.
 
+	input:
+	>>> ip = 'wow:Good:2_two:five: hi-2 bye kite.777.water.'
+
+	output:
+	'wow hi-2 bye kite'
+	"""
+	regex = re.compile(r'(\w+)[:.](\w+[:.])+')
+	return regex.sub('\g<1>', ip)
+def split(ip):
+	"""
+	Split the given input string on one or more repeated sequence of cat.
+
+	input:
+	>>> ip = 'firecatlioncatcatcatbearcatcatparrot'
+
+	output:
+	['fire', 'lion', 'bear', 'parrot']
+	"""
+	regex = re.compile(r'(?:cat)+')
+	return regex.split(ip)
+def find(ip):
+	"""
+	For the given input string, find all occurrences of digit sequences with
+	at least one repeating sequence. For example, '232323' and '897897'. If the
+	repeats end prematurely, for example '12121', it should not be matched.
+
+	input:
+	>>> ip = '1234 2323 453545354535 9339 11 60260260'
+
+	output:
+	['2323', '453545354535', '11']
+	['23', '4535', '1']
+	"""
+	regex = re.compile(r'\b(\d+)\1+\b')
+	return [x[0] for x in regex.finditer(ip)],regex.findall(ip)
+def convert(ip):
+	"""
+	Convert the comma separated strings to corresponding 'dict' objects as shown below.
+	The keys are 'name', 'maths' and 'phy' for the three fields in the input strings.
+
+	input:
+	>>> row1 = 'rohan,75,89'
+	>>> row2 = 'rose,88,92'
+
+	output:
+	{'name': 'rohan', 'maths': '75', 'phy': '89'}
+	{'name': 'rose', 'maths': '88', 'phy': '92'}
+	"""
+	regex = re.compile(r'(\w+),(\d+),(\d+)')
+	return {'name':regex.search(ip)[1],'maths':regex.search(ip)[2],
+	'phy':regex.search(ip)[3]}
+def surround(ip):
+	"""
+	Surround all whole words with ().
+	Additionally, if the whole word is imp or ant, delete them.
+	Can you do it with single substitution?
+
+	input:
+	ip = 'tiger imp goat eagle ant important'
+
+	output:
+	'(tiger) () (goat) (eagle) () (important)'
+	"""
+	regex1 = re.compile(r'(\b\w+\b)')
+	regex2 = re.compile(r'(\(imp\)|\(ant\))')
+	sub1 = regex1.sub('(\g<1>)', ip)
+	return regex2.sub('()', sub1)
+def filter(ip):
+	"""
+	Filter all elements that contains a sequence of lowercase
+	alphabets followed by - followed by digits. They can be optionally
+	surrounded by {{ and }}. Any partial match shouldn't be part of the output.
+
+	input:
+	>>> ip = ['{{apple-150}}', '{{mango2-100}}', '{{cherry-200', 'grape-87']
+
+	output:
+	['{{apple-150}}', 'grape-87']
+	"""
+	regex = re.compile(r'({{[a-z]+-\d+}})|(\w+-\d+)')
+	return [x for x in ip if regex.fullmatch(x)]
+def display(ip):
+	"""
+	The given input string has sequences made up of words separated by : or .
+	and such sequences will end when : or . is not followed by a word character.
+	For all such sequences, display only the last word followed by - followed by
+	first word.
+
+	input:
+	>>> ip = 'wow:Good:2_two:five: hi-2 bye kite.777.water.'
+
+	output:
+	['five-wow', 'water-kite']
+	"""
+	regex = re.compile(r'(\w+)[:.](?:(\w+)[:.])+')
+	return [x.expand(r'\2-\1') for x in regex.finditer(ip)]
+
+a = display('wow:Good:2_two:five: hi-2 bye kite.777.water.')
+print(a)
 
 
