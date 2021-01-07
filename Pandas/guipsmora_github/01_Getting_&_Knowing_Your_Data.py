e7d297e584cd8ebe4569c95f1a8b7a555b73b3fd
margolek <margolek555@gmail.com>
2021-01-07 16:41:42 +0100

    Update

---
+++ Pandas/guipsmora_github/01_Getting_&_Knowing_Your_Data.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+
+food = pd.read_csv(r"C:\Users\margo\OneDrive\Dokumenty\GitHub_data\archive\en.openfoodfacts.org.products.tsv",sep='\t')
+
+print(food.head())
